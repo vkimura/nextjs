f4101c633360fccfe94d23cc7eb564a0bc04cc97
Victor Kimura <vkimura@gmail.com>
2022-08-25 21:14:13 -0700

    Updates hero css

---
+++ 02-next-tailwind/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
+        Hero
+        {/* Overlay */}
+        <div />
+        <div>
+            <h2>Heading</h2>
+            <p>Message</p>
+            <button>Book</button>
+        </div>
+    </div>
+  );
+};
+
+export default Hero;
--- 02-next-tailwind/pages/_document.js
+++ 02-next-tailwind/pages/_document.js
@@ -1,7 +1,6 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-  render() {
+export default function Document() {
     return (
       <Html>
         <Head>
@@ -10,7 +9,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,700;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
             rel="stylesheet"
@@ -22,7 +21,4 @@ class MyDocument extends Document {
         </body>
       </Html>
     );
-  }
 }
-
-export default MyDocument;
